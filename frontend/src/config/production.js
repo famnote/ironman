be909e58b6c990fcd694bff0e84e7d6af28866fc
nevertry <nevertry@famnote.com>
2020-01-13 18:00:26 +0900

    frontend firebaseConfig 수정

--- frontend/src/config/production.js
+++ frontend/src/config/production.js
@@ -1,5 +1,12 @@
 const firebaseConfig = {
-
+  apiKey: "AIzaSyCItWFGwjHBM2ym1fpxCDgXFU3XOMjXigM",
+  authDomain: "hangover-2020.firebaseapp.com",
+  databaseURL: "https://hangover-2020.firebaseio.com",
+  projectId: "hangover-2020",
+  storageBucket: "hangover-2020.appspot.com",
+  messagingSenderId: "565712083476",
+  appId: "1:565712083476:web:fb4424da2c15ffdf6ae501",
+  measurementId: "G-D74JRKBFSX"
 };
 
 // Cloud Functions
