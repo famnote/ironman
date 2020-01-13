347d4e03654ed147cf57adfa46cabbb60d96d730
nevertry <nevertry@famnote.com>
2020-01-13 15:23:21 +0900

    gitignore for frontend config 4

--- frontend/src/config/development.js
+++ frontend/src/config/development.js
@@ -1,17 +1,5 @@
 const firebaseConfig = {
-<<<<<<< HEAD
-  apiKey: "AIzaSyCItWFGwjHBM2ym1fpxCDgXFU3XOMjXigM",
-  authDomain: "hangover-2020.firebaseapp.com",
-  databaseURL: "https://hangover-2020.firebaseio.com",
-  projectId: "hangover-2020",
-  storageBucket: "hangover-2020.appspot.com",
-  messagingSenderId: "565712083476",
-  appId: "1:565712083476:web:fb4424da2c15ffdf6ae501",
-  measurementId: "G-D74JRKBFSX"
-  
-=======
 
->>>>>>> d99de037a39d04e9cac1603825de708ee8f503c1
 };
 
 // Cloud Functions
