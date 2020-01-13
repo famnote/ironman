d99de037a39d04e9cac1603825de708ee8f503c1
Wayne Park <nevertry@famnote.com>
2020-01-13 15:12:56 +0900

    Create localhost.js

---
+++ frontend/src/config/localhost.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+
+};
+
+// Cloud Functions
+// const backendUrl = `http://localhost:5000/${
+//   firebaseConfig.projectId
+// }/us-central1/api/api`;
+
+// App Engine / Debug
+const backendUrl = `http://localhost:8080/api`;
+
+export default {
+  firebaseConfig,
+  backendUrl,
+};
